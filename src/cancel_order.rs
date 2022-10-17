d125009bacb6650e393d19fdd536f131d56bfeb5
Tymofii <timofii.savinov@gmail.com>
2022-10-17 13:47:06 +0300

    cancel order in progress

--- src/cancel_order.rs
+++ src/cancel_order.rs
@@ -1,6 +1,37 @@
+use near_sdk::env::signer_account_id;
 use crate::*;
+use crate::big_decimal::BigDecimal;
 
 #[near_bindgen]
 impl Contract {
-    fn cancel_order(&mut self, order_id: U128, swap_fee: U128, price_impact: U128) {}
+    fn cancel_order(&mut self, order_id: U128, swap_fee: U128, price_impact: U128) {
+        let orders = self.orders.get(&signer_account_id()).unwrap_or_else(||{
+           panic!("Orders for account: {} not found", signer_account_id());
+        });
+
+        let order = orders.get(&(order_id.0 as u64)).unwrap_or_else(|| {
+            panic!("Order with id: {} not found", order_id.0);
+        });
+
+        if order.status == OrderStatus::Pending {
+            //remove liquadity
+        }
+
+        if order.leverage > BigDecimal::from(1) {
+
+        }
+
+        let market_data = self.market_infos.get().unwrap();
+        let pnl = self.calculate_pnl(signer_account_id(), order_id, market_data);
+
+        if pnl.is_profit {
+
+        }
+
+        self.update_balance() {
+
+        }
+
+        self.
+    }
 }
--- src/market.rs
+++ src/market.rs
@@ -3,14 +3,14 @@ use crate::*;
 #[near_bindgen]
 impl Contract {
     #[private]
-    pub fn add_pair(&mut self, pairData: TradePair) {
-        let pair = (pairData.sell_token.clone(), pairData.buy_token.clone());
-        self.supported_markets.insert(&pair, &pairData);
+    pub fn add_pair(&mut self, pair_data: TradePair) {
+        let pair = (pair_data.sell_token.clone(), pair_data.buy_token.clone());
+        self.supported_markets.insert(&pair, &pair_data);
     }
 
     #[private]
-    pub fn remove_pair(&mut self, pairData: TradePair) {
-        let pair = (pairData.sell_token.clone(), pairData.buy_token.clone());
+    pub fn remove_pair(&mut self, pair_data: TradePair) {
+        let pair = (pair_data.sell_token.clone(), pair_data.buy_token.clone());
         self.supported_markets.remove(&pair);
     }
 }

025417d2ec141356bec6108e53194ff7b83570e8
Tymofii <timofii.savinov@gmail.com>
2022-10-21 17:11:40 +0300

    hot fix

--- src/create_order.rs
+++ src/create_order.rs
@@ -1,9 +1,18 @@
+use crate::big_decimal::WBalance;
 use crate::*;
 
 #[near_bindgen]
 impl Contract {
-    pub fn create_order(&mut self, order_type: String, amount: WBalance, sell_token: AccountId, buy_token: AccountId, leverage: U128) {}
+    pub fn create_order(
+        &mut self,
+        order_type: String,
+        amount: WBalance,
+        sell_token: AccountId,
+        buy_token: AccountId,
+        leverage: U128,
+    ) {
+    }
 
     #[private]
-    pub fn add_order(&mut self, account_id: AccountId, order: Order ) {}
+    pub fn add_order(&mut self, account_id: AccountId, order: Order) {}
 }
---
+++ src/execute_order.rs
@@ -0,0 +1,6 @@
+use crate::*;
+
+#[near_bindgen]
+impl Contract {
+    pub fn execute_order(order_id: U128) {}
+}

497dec5fdcd0c1beb872e20a828ca8fc09ddbcc0
vlad-sundunchan <110816240+vlad-sundunchan@users.noreply.github.com>
2022-11-04 15:10:09 +0300

    Create get liquidation price (#11)
    * Create get liquidation price
    
    * Create get liquidation price
    
    * Create calculate liquidation price
    
    * Removed calculate liquidation price
    
    * Created calculate liquidation price
    
    * Created calculate liquidation price
    
    * Created calculate liquidation price
    
    * Created set volatility rate. Added volatility rate field
    
    * Created set volatility rate. Added volatility rate field
    
    Co-authored-by: Mark Tsyrulnyk <91728093+markblaize@users.noreply.github.com>

--- src/view.rs
+++ src/view.rs
@@ -176,6 +176,32 @@ impl Contract {
     pub fn view_liquidation_threshold(&self) -> U128 {
         U128(self.liquidation_threshold)
     }
+
+    pub fn calculate_liquidation_price(
+        &self,
+        sell_token_amount: U128,
+        sell_token_price: Price,
+        buy_token_price: Price,
+        leverage: U128,
+        borrow_fee: U128,
+        swap_fee: U128,
+    ) -> Price {
+        let collateral_usd =
+            BigDecimal::from(sell_token_amount) * BigDecimal::from(sell_token_price.value);
+        let position_amount_usd = collateral_usd * BigDecimal::from(leverage.0);
+        let borrow_amount = collateral_usd * (BigDecimal::from(leverage.0) - BigDecimal::from(1));
+        let buy_amount = position_amount_usd / BigDecimal::from(buy_token_price.value);
+
+        let liquidation_price = (position_amount_usd - self.volatility_rate * collateral_usd
+            + borrow_amount * BigDecimal::from(borrow_fee)
+            + position_amount_usd * BigDecimal::from(swap_fee))
+            / buy_amount;
+
+        Price {
+            ticker_id: "usd".to_string(),
+            value: liquidation_price,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -313,4 +339,74 @@ mod tests {
         assert!(pnl.is_profit);
         assert_eq!(pnl.amount, U128(256558246105184350685977637895071072256));
     }
+
+    #[test]
+    fn test_calculate_liquidation_price_sell_usdt() {
+        let contract = Contract::new_with_config(
+            "owner_id.testnet".parse().unwrap(),
+            "oracle_account_id.testnet".parse().unwrap(),
+        );
+
+        let sell_token_price = Price {
+            ticker_id: "usdt".to_string(),
+            value: BigDecimal::from(U128(10_u128.pow(24))),
+        };
+    
+        let buy_token_price = Price {
+            ticker_id: "wnear".to_string(),
+            value: BigDecimal::from(U128(10_u128.pow(25))),
+        };
+    
+        let result = contract.calculate_liquidation_price(
+            U128(10_u128.pow(27)),
+            sell_token_price,
+            buy_token_price,
+            U128(3),
+            U128(5 * 10_u128.pow(22)),
+            U128(3 * 10_u128.pow(20)),
+        );
+    
+        assert_eq!(
+            (result.ticker_id, result.value),
+            (
+                "usd".to_string(),
+                BigDecimal::from(U128(7169666666666666666666666))
+            )
+        );
+    }
+    
+    #[test]
+    fn test_calculate_liquidation_price_sell_wnear() {
+        let contract = Contract::new_with_config(
+            "owner_id.testnet".parse().unwrap(),
+            "oracle_account_id.testnet".parse().unwrap(),
+        );
+
+        let sell_token_price = Price {
+            ticker_id: "wnear".to_string(),
+            value: BigDecimal::from(U128(10_u128.pow(25))),
+        };
+    
+        let buy_token_price = Price {
+            ticker_id: "usdt".to_string(),
+            value: BigDecimal::from(U128(10_u128.pow(24))),
+        };
+    
+        let result = contract.calculate_liquidation_price(
+            U128(10_u128.pow(27)),
+            sell_token_price,
+            buy_token_price,
+            U128(2),
+            U128(5 * 10_u128.pow(22)),
+            U128(3 * 10_u128.pow(20)),
+        );
+    
+        assert_eq!(
+            (result.ticker_id, result.value),
+            (
+                "usd".to_string(),
+                BigDecimal::from(U128(5503 * 10_u128.pow(20)))
+            )
+        );
+    }  
 }
